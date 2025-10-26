900745e6c23f4df3ad98cee00ea14d0a3465ecf0
Kirankumar G <112302179+Kirankumar2604@users.noreply.github.com>
2025-10-26 19:53:28 +0530

    main

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = 3000;
+
+app.use(cors())
+
+app.get("/", (req,res)=>{
+    res.send("Hello from Node backend!")
+});
+
+app.get("/api/message",(res,req)=>{
+    req.json({message:"Hello from Express API!"});
+});
+
+app.listen(PORT, ()=>{
+    console.log(`✅ Server is running on http://localhost:${PORT}`);
+});
