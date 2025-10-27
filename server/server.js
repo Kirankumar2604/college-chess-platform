c7f1a45fd8032af3373649dddebe04f8c8a08a85
Kirankumar G <112302179+Kirankumar2604@users.noreply.github.com>
2025-10-27 23:22:34 +0530

    main

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import User from "./src/models/User.js"
 
 dotenv.config();
 
@@ -15,6 +16,30 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.get("/", (req, res) => {
   res.json({ status: "Backend OK" });
+});
+
+app.post("/api/users", async (req,res)=>{
+    try {
+    const { regNo, email } = req.body;
+
+    // Check if the user already exists
+    const existingUser = await User.findOne({ regNo });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    // Create a new user
+    const newUser = new User({
+      regNo,
+      email,
+    });
+
+    await newUser.save();  // Save the user to MongoDB
+    res.status(201).json(newUser);  // Return the created user as the response
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
 });
 
 const PORT = process.env.PORT || 5000;
